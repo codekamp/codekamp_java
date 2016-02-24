a00bbf2adb2b3684a92f148c120e3c71772f247d
Prashant Chaudhary <101.prashant@gmail.com>
2016-02-24 18:39:29 +0530

    added game code

--- GraphicsDemo/src/CodeKampPanel.java
+++ GraphicsDemo/src/CodeKampPanel.java
@@ -1,30 +1,104 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 
 /**
  * Created by cerebro on 19/02/16.
  */
-public class CodeKampPanel extends JPanel {
+public class CodeKampPanel extends JPanel implements KeyListener, MouseListener {
 
-    public int xCord = 150;
-    public int yCord = 0;
+    public int playerXCord = 300;
+    public int playerYCord = 315;
+    public int playerYVel = 0;
+    public int playerYAcc = 0;
+    public int blockXCord = 700;
+    public int blockYCord = 355;
+    public boolean blockVisible = true;
     public BufferedImage grassImage = null;
     public BufferedImage playerImage = null;
+    public BufferedImage blockImage = null;
+    public boolean isRunning = true;
+    public boolean isGameOver = false;
+
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
 
-        g.setColor(Color.blue);
-        g.fillRect(0,0,800,450);
+        if (isGameOver) {
+            g.setColor(Color.RED);
+            g.drawString("Game Over", 200, 400);
+        } else {
 
-        if (grassImage != null) {
-            g.drawImage(grassImage, 0, 405, null);
-        }
+            g.setColor(Color.blue);
+            g.fillRect(0, 0, 800, 450);
 
-        if (playerImage != null) {
-            g.drawImage(playerImage, 300, 315, null);
+            if (grassImage != null) {
+                g.drawImage(grassImage, 0, 405, null);
+            }
+
+            if (playerImage != null) {
+                g.drawImage(playerImage, playerXCord, playerYCord, null);
+            }
+
+            if (blockImage != null && this.blockVisible) {
+                g.drawImage(blockImage, blockXCord, blockYCord, null);
+            }
+
+            g.setColor(Color.yellow);
+            g.fillRect(700, 10, 50, 50);
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE && this.playerYCord == 315) {
+            this.playerYVel = -20;
+            this.playerYAcc = 1;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        int xCordOfCLick = e.getX();
+        int yCordOfCLick = e.getY();
+
+        if (xCordOfCLick > 700 && xCordOfCLick < 750 && yCordOfCLick > 10 && yCordOfCLick < 60) {
+            this.isRunning = !this.isRunning;
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
--- GraphicsDemo/src/Demo.java
+++ GraphicsDemo/src/Demo.java
@@ -1,11 +1,9 @@
-import com.sun.org.apache.bcel.internal.classfile.Code;
+
 
 import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageInputStream;
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.net.URL;
+import java.util.Random;
 
 /**
  * Created by cerebro on 19/02/16.
@@ -24,97 +22,107 @@ public class Demo {
         frame1.pack();
         frame1.setResizable(false);
         frame1.setVisible(true);
+        panel1.setFocusable(true);
+        panel1.requestFocus();
 
-        //code for loading image from URL
-//        try {
-//            URL url = new URL("IMAGE_URL_HERE");
-//            panel1.image = ImageIO.read(url);
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
+        panel1.addKeyListener(panel1);
+        panel1.addMouseListener(panel1);
 
         try {
             panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim1.png"));
             panel1.grassImage = ImageIO.read(Demo.class.getResourceAsStream("/grass.png"));
+            panel1.blockImage = ImageIO.read(Demo.class.getResourceAsStream("/block.png"));
             panel1.repaint();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
         int count = 0;
-        while (true) {
-            count++;
-            count = count % 8;
+        Random random = new Random();
+        Rectangle playerRect;
+        Rectangle blockRect;
+        while (!panel1.isGameOver) {
+            if(panel1.isRunning) {
+                count++;
+                count = count % 8;
 
-            try {
+                try {
 
-                switch (count) {
-                    case 0:
-                        panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim1.png"));
-                        break;
-                    case 1:
-                        panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim2.png"));
-                        break;
-                    case 2:
-                        panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim3.png"));
-                        break;
-                    case 3:
-                        panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim4.png"));
-                        break;
-                    case 4:
-                        panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim5.png"));
-                        break;
-                    case 5:
-                        panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim4.png"));
-                        break;
-                    case 6:
-                        panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim3.png"));
-                        break;
-                    case 7:
-                        panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim2.png"));
-                        break;
+                    switch (count) {
+                        case 0:
+                            panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim1.png"));
+                            break;
+                        case 1:
+                            panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim2.png"));
+                            break;
+                        case 2:
+                            panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim3.png"));
+                            break;
+                        case 3:
+                            panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim4.png"));
+                            break;
+                        case 4:
+                            panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim5.png"));
+                            break;
+                        case 5:
+                            panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim4.png"));
+                            break;
+                        case 6:
+                            panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim3.png"));
+                            break;
+                        case 7:
+                            panel1.playerImage = ImageIO.read(Demo.class.getResourceAsStream("/run_anim2.png"));
+                            break;
+                    }
+
+                    panel1.playerYCord += panel1.playerYVel;
+                    panel1.playerYVel += panel1.playerYAcc;
+
+                    if (panel1.playerYCord >= 315) {
+                        panel1.playerYCord = 315;
+                        panel1.playerYAcc = 0;
+                        panel1.playerYVel = 0;
+                    }
+
+                    panel1.blockXCord -= 5;
+
+                    if(panel1.blockXCord < -20) {
+                        panel1.blockXCord = 850;
+                        if(random.nextInt(2) == 0) {
+                            panel1.blockYCord = 355;
+                        } else {
+                            panel1.blockYCord = 280;
+                        }
+                        panel1.blockVisible = true;
+                    }
+
+
+                    playerRect = new Rectangle(panel1.playerXCord, panel1.playerYCord, 72, 90);
+                    blockRect = new Rectangle(panel1.blockXCord, panel1.blockYCord, 20, 50);
+
+                    if(panel1.blockVisible && playerRect.intersects(blockRect)) {
+                        panel1.playerXCord -= 100;
+                        panel1.blockVisible = false;
+                    }
+
+                    if (panel1.playerXCord < 0) {
+                        panel1.isGameOver = true;
+                    }
+
+                    panel1.repaint();
+
+                } catch (Exception e) {
+
                 }
 
-                panel1.repaint();
-                Thread.sleep(15);
-            } catch (Exception e) {
 
             }
-        }
 
-//        int xVel = 1;
-//        int yVel = 1;
-//        while (true) {
-//            panel1.xCord = panel1.xCord + xVel;
-//            panel1.yCord = panel1.yCord + yVel;
-//
-//            if(panel1.yCord > 400) {
-//                panel1.yCord = 400;
-//                yVel = -1;
-//            }
-//
-//            if(panel1.xCord > 750) {
-//                panel1.xCord = 750;
-//                xVel = -1;
-//            }
-//
-//            if(panel1.yCord < 0) {
-//                panel1.yCord = 0;
-//                yVel = 1;
-//            }
-//
-//            if(panel1.xCord < 0) {
-//                panel1.xCord = 0;
-//                xVel = 1;
-//            }
-//
-//            panel1.repaint();
-//
-//            try {
-//                Thread.sleep(3);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
+            try {
+                Thread.sleep(15);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
